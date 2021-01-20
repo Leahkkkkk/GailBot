c71e52e17d39cfe12d0d4daad0ad68efb0aee91c
Muhammad Umair <umair.aarij@gmail.com>
2021-01-20 18:14:00 -0500

    Separated IDictModel class as a separate util.

--- Src/Components/network/WS_models.py
+++ Src/Components/network/WS_models.py
@@ -1,5 +1,7 @@
 # Standard library imports 
 from typing import Any, Tuple
+# Local imports 
+from ...utils.models import IDictModel
 # Third party imports
 from enum import Enum
 
@@ -22,7 +24,7 @@ class WSProtocolAttributes(Enum):
     callback_data_parameter = "data_parameter"
     callback_return_data = "return_data"
 
-class WebsocketProtocolModel:
+class WebsocketProtocolModel(IDictModel):
     """
     This model class contains information that can be passed to callbacks during 
     different stages of a websocket connection.
@@ -33,48 +35,12 @@ class WebsocketProtocolModel:
             items (Dict): Contains mapping of WSProtocolAttributes to their 
                             values.
         """
+        super().__init__()
         self.items = {
             WSProtocolAttributes.send_close_callback : None,
             WSProtocolAttributes.send_message_callback : None,
             WSProtocolAttributes.send_ping_callback : None, 
             WSProtocolAttributes.send_pong_callback : None,
             WSProtocolAttributes.callback_return_data : None, 
-            WSProtocolAttributes.callback_data_parameter : None
-        }
-    
-    def get(self, attr : str) -> Tuple[bool,Any]:
-        """
-        Returns the value associated with the attribute if that attribute exists
-        in WSProtocolAttributes.
-
-        Args:
-            attr (WSProtocolAttributes)
-        
-        Returns:
-            (Tuple[bool,Any]): True + data is attribute exists.
-                            False + None if the attribute is invalid.
-        """
-        try:
-            return (True,self.items[attr])
-        except:
-            return (False, None) 
-
-    def set(self, attr : str, data : Any) -> bool:
-        """
-        Sets the given attribute to the data if it exists in WSProtocolAttributes.
-
-        Args:
-            attr (WSProtocolAttributes)
-            data (Any): Data associated with the attribute.
-        
-        Returns:
-            (bool): true if successful. False otherwise.
-        """
-        if attr in self.items.keys():
-            self.items[attr] = data  
-            return True 
-        return False  
-
-    def count(self) -> int:
-        return len(self.items.keys())
+            WSProtocolAttributes.callback_data_parameter : None}
 
