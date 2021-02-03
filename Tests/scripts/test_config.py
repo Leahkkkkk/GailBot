99602f46112be460ef9ab557cc3b9ef5daddc579
Rosa Vitiello <rosanna.m.vitiello@gmail.com>
2021-02-03 15:21:29 -0500

    Modified tests for new Config component structure

--- Tests/scripts/test_config.py
+++ Tests/scripts/test_config.py
@@ -1,210 +1,244 @@
 
 # Local imports 
-from Src.Components.config import BlackBoard, BlackBoardAttributes, Config
+from Src.Components.config import BlackBoard, SystemBB, SystemBBAttributes, Config
 from ..suites import TestSuite, TestSuiteAttributes
 
-############################### GLOBALS #####################################
-VALID_CONFIG_FILE_PATH = "Tests/Test_files/Others/sample_config.json"
-INVALID_CONFIG_FILE_PATH = "Tests/Test_files/Others/invalid_sample_config.json"
-INVALID_JSON_FILE_PATH = "Tests/Test_files/Others/invalid_json.json"
-TEXTFILE_FILE_PATH = "Tests/Test_files/Others/textfile.txt"
-INVALID_DUPLICATE_FILE_PATH = "Tests/Test_files/Others/duplicate_key.json"
-EXTRA_KEYS_JSON_FILE_PATH = "Tests/Test_files/Others/extra_keys.json"
 ########################## TEST DEFINITIONS ##################################
-
+#TODO: Define more blackboards to test with more complex data.
 
 #### BLACKBOARD TESTS
 
-def blackboard_set() -> bool:
+def blackboard_is_configured_true() -> bool:
     """
-    Tests the set function in blackboard. 
+    Tests blackboard configure with good data.
 
     Tests:
-        1. Add a key-value pair where key is not in BlackBoardAttributes.
-        2. Add a key-value pair where key is in BlackBoardAttributes.
+        1. Load valid data into SystemBB.
+        2. Confirm board is configured.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    blackboard_data = { "Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return blackboard.is_configured()
+
+def blackboard_is_configured_false() -> bool:
+    """
+    Tests blackboard configure with bad data.
+
+    Tests: 
+        1. Load invalid dictionary data into SystemBB.
+        2. Confirm board is not configured.
 
     Result:
         (bool): True if all the tests pass. False otherwise.
     """
-    blackboard = BlackBoard()
-    return not blackboard.set("Dummy key", None) and \
-            blackboard.set(BlackBoardAttributes.Test_key, "Test String")
+    blackboard_data = { "Not_a_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return not blackboard.is_configured()
+
+def blackboard_no_configure_with_bad_data() -> bool:
+    """
+    Tests blackboard configure with bad data.
+
+    Tests: 
+        1. Load invalid non-dictionary data into SystemBB.
+        2. Confirm board is not configured.
+
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+
+    blackboard_data = {"This is bad data"}
+    blackboard = SystemBB(blackboard_data)
+    return not blackboard.is_configured()
 
 def blackboard_set_invalid_key() -> bool:
     """
     Tests set function of invalid key in blackboard.
 
     Tests:
-        1. Add a key-value pair where key is not in BlackBoardAttributes.
+        1. Load valid data into SystemBB.
+        2. Set a key-value pair where key is not in SystemBBAttributes.
 
     Result:
         (bool): True if all the tests pass. False otherwise.
     """
 
-    blackboard = BlackBoard()
+    blackboard_data = { "Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
     return not blackboard.set("Dummy key", None)
 
 def blackboard_set_valid_key() -> bool:
     """
-    Tests set function of valid key in blackboard
+    Tests set function of valid key in blackboard.
 
     Tests:
-        1. Add a key-value pair where key is in BlackBoardAttributes
+        1. Load valid data into SystemBB.
+        2. Set a key-value pair where key is in SystemBBAttributes.
 
     Results:
         (bool): True if all the tests pass. False otherwise.
     """
+    blackboard_data = { "Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return blackboard.set(SystemBBAttributes.Test_key, "Test")
 
-    blackboard = BlackBoard()
-    return blackboard.set(BlackBoardAttributes.Test_key, "Test String")
-
-def blackboard_get() -> bool:
+def blackboard_get_configured_key() -> bool: #TODO: fix this!
     """
-    Tests get function in BlackBoard 
+    Test get function with a valid unset key from SystemBBAttributes.
 
     Tests:
-        1. Retrieve a key that does not exist in the blackboard but is in 
-            BlackBoardAttributes.
-        2. Retrieve a key that does exist in the blackboard and is in 
-            BlackBoardAttributes.  
-        3. Retrieve using a key that does not exist in BlackBoardAttributes.
+        1. Load data into SystemBB.
+        2. Get attribute that was loaded into the blackboard.
     
     Result:
         (bool): True if all the tests pass. False otherwise.
     """
-    blackboard = BlackBoard()
-    return blackboard.get(BlackBoardAttributes.Test_key) == None and \
-        (blackboard.set(BlackBoardAttributes.Test_key,"Test") and \
-        blackboard.get(BlackBoardAttributes.Test_key) == "Test") and \
-        not blackboard.get("Dummy key")
+    blackboard_data = {"Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return blackboard.get(SystemBBAttributes.Test_key) == (True, "I am a test string")
 
-def blackboard_get_valid_unset_key() -> bool:
-    blackboard = BlackBoard()
-    return blackboard.get(BlackBoardAttributes.Test_key) == None
+def blackboard_get_set_key() -> bool:
+    """
+    Test get function with a valid set key from SystemBBAttributes.
 
-def blackboard_get_valid_set_key() -> bool:
-    blackboard = BlackBoard()
-    return (blackboard.set(BlackBoardAttributes.Test_key,"Test") and \
-        blackboard.get(BlackBoardAttributes.Test_key) == "Test")
+    Tests:
+        1. Load data into SystemBB.
+        2. Set an attribute that exists in SystemBBAttributes.
+        3. Get attribute that was set and confirm that the attribute was 
+           set correctly.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    blackboard_data = {"Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return (blackboard.set(SystemBBAttributes.Test_key,"Test") and \
+        blackboard.get(SystemBBAttributes.Test_key) == (True, "Test"))
 
 def blackboard_get_invalid_key() -> bool:
-    blackboard = BlackBoard()
-    return not blackboard.get("Dummy key")
+    """
+    Test get function with an invalid key from SystemBBAttributes.
+
+    Tests:
+        1. Load data in SystemBB.
+        2. Check failture to retrieve using a key that does not exist in 
+           SystemBBAttributes.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    blackboard_data = {"Test_key" : "I am a test string"}
+    blackboard = SystemBB(blackboard_data)
+    return blackboard.get("Dummy key") == (False, None)
 
 #### CONFIG TESTS
 
-def config_load_from_file() -> bool:
+def config_load_into_nonexistent_board() -> bool:
     """
-    Tests the load from file function in config.
+    Tests loading an invalid blackboard type in Config.
 
     Tests:
-        1. Load a file that does not exist and the format is valid.
-        2. Load a file that does not exist and the format is not valid.
-        3. Load a file that does exist and the format is valid and the 
-            config file keys are also valid.
-        4. Load a file that does exist and the format is not valid. 
-        5. Load a file that does exist and the format is valid but the config 
-            keys are invalid.
-        6. Load a file that does exist and the format is valid, but the 
-            text in the file is not in the expected format.
-        7. Load a file that does exist but the format arg and the actual 
-            type of file do not match
-        8. Load a file that does exist, format is valid, and there are extra
-           keys
-        9. Load a file that does exist and format is valid, but there are
-            duplicate keys
+        1. Loads data into an undefined blackboard.
+        2. Confirms that load was unsucessful.
     
     Result:
         (bool): True if all the tests pass. False otherwise.
     """
     config = Config()
-    return not config.load_from_file("Dummy", "JSON") and \
-        not config.load_from_file("Dummy", "YAML") and \
-        config.load_from_file(VALID_CONFIG_FILE_PATH,"JSON") and \
-        not config.load_from_file(VALID_CONFIG_FILE_PATH,"YAML") and \
-        not config.load_from_file(INVALID_CONFIG_FILE_PATH, "JSON") and \
-        not config.load_from_file(INVALID_JSON_FILE_PATH, "JSON") and \
-        not config.load_from_file(TEXTFILE_FILE_PATH, "JSON") and \
-        config.load_from_file(EXTRA_KEYS_JSON_FILE_PATH, "JSON") and \
-        not config.load_from_file(INVALID_DUPLICATE_FILE_PATH, "JSON")
+    return not config.load_blackboard("notABoard", {"Key" : "key"})
 
-def config_load_from_valid_nonexisting_file() -> bool:
-    config = Config()
-    return not config.load_from_file("Dummy", "JSON")
-
-def config_load_from_invalid_nonexisting_file() -> bool:
-    config = Config()
-    return not config.load_from_file("Dummy", "YAML")
-
-def config_load_from_valid_existing_file() -> bool:
-    config = Config()
-    return config.load_from_file(VALID_CONFIG_FILE_PATH,"JSON")
-
-def config_load_from_invalid_existing_file() -> bool:
-    config = Config()
-    return not config.load_from_file(VALID_CONFIG_FILE_PATH,"YAML")
-
-def config_load_from_invalid_key_file() -> bool:
-    config = Config()
-    return not config.load_from_file(INVALID_CONFIG_FILE_PATH, "JSON")
-
-def config_load_from_invalid_JSON() -> bool:
-    config = Config()
-    return not config.load_from_file(INVALID_JSON_FILE_PATH, "JSON")
-
-def config_load_from_incorrect_format_file() -> bool:
-    config = Config()
-    return not config.load_from_file(TEXTFILE_FILE_PATH, "JSON")
-
-def config_load_from_JSON_with_extra_keys() -> bool:
-    config = Config()
-    return config.load_from_file(EXTRA_KEYS_JSON_FILE_PATH, "JSON")
-
-def config_load_from_JSON_with_duplicates() -> bool:
-    config = Config()
-    return not config.load_from_file(INVALID_DUPLICATE_FILE_PATH, "JSON")
-
-def config_get_blackboard() -> bool:
+def config_load_into_existent_board() -> bool:
     """
-    Tests the get_blackboard function in config 
+    Tests sucessful load into a valid blackboard type in Config.
+
+    Tests
+        1. Loads valid data into a defined SystemBB.
+        2. Confirms that load was sucessful.
+
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    config = Config()
+    return config.load_blackboard("system_blackboard", {"Test_key" : "key"})
+
+def config_load_with_bad_data() -> bool:
+    """
+    Tests unsucessful load with invalid data in a valid blackboard type in 
+    Config.
 
     Tests:
-        1. Get blackboard without loading file.
-        2. Get blackboard after loading file.
-        3. Check to make sure that the same attribute from both blackboard is 
-            not the same.
-        4. Check to make sure that the attribute in the initialized blackboard 
-            is set correctly
-        5. Check to make sure that config.get_blackboard() reflects changes
+        1. Loads invalid data into SystemBB.
+        2. Confirms that load was unsucessful.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
     """
-    config = Config()  
-    uninitialized_blackboard =  config.get_blackboard()
-    config.load_from_file(VALID_CONFIG_FILE_PATH,"JSON")
-    initialized_blackboard = config.get_blackboard()
-    return uninitialized_blackboard.get(BlackBoardAttributes.Test_key) != \
-            initialized_blackboard.get(BlackBoardAttributes.Test_key) and \
-           initialized_blackboard.get(BlackBoardAttributes.Test_key) == \
-            "I am a test string" and \
-           config.get_blackboard().get(BlackBoardAttributes.Test_key) == \
-            "I am a test string"
+    config = Config()
+    return not config.load_blackboard("system_blackboard", {"bad data!"})
 
-def config_blackboard_deepcopy() -> bool:
+def config_load_with_bad_key() -> bool:
     """
-    Tests that get_blackboard return is a deep copy
+    Tests unsuccessful load with invalid key data in a valid blackboard type in 
+    Config.
 
     Tests:
-        1. Get blackboard without loading file.
-        2. Set blackboard attribute
-        3. Check to make sure that the config.get_blackboard() does not reflect
-            changes
+        1. Loads invalid dictionary into SystemBB.
+        2. Confirms that load was unsucessful.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
     """
-
     config = Config()
-    blackboard = config.get_blackboard()
-    blackboard.set(BlackBoardAttributes.Test_key, "Hello!")
-    return blackboard.get(BlackBoardAttributes.Test_key) == "Hello!" and \
-           config.get_blackboard().get(BlackBoardAttributes.Test_key) != \
-            "Hello!"
+    return not config.load_blackboard("system_blackboard", {"Bad_key" : "key"})
+
+def config_get_nonexistent_board() -> bool:
+    """
+    Tests get of invalid blackboard type in Config.
+
+    Tests:
+        1. Gets blackboard with invalid type.
+        2. Confirms get was unsucessful.
+    
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    config = Config()
+    return config.get_blackboard("notABoard") == (False, None)
+
+def config_get_existent_unloaded_board() -> bool:
+    """
+    Tests get of unloaded valid blackboard type in Config,
+
+    Tests:
+        1. Gets unloaded blackboard with valid type.
+        2. Confirms get was unsucessful.
+
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    config = Config()
+    return config.get_blackboard("system_blackboard") == (False, None)
+
+def config_get_loaded_board() -> bool:
+    """
+    Tests sucessful get of loaded valid blackboard type in Config.
+
+    Tests: 
+        1. Loads valid blackboard data in SystemBB.
+        2. Gets SystemBB.
+        3. Confirms get was sucessful and the board was set properly.
+
+    Result:
+        (bool): True if all the tests pass. False otherwise.
+    """
+    config = Config()
+    config.load_blackboard("system_blackboard", {"Test_key" : "key"})
+    success, system_bb = config.get_blackboard("system_blackboard")
+    return success and \
+           system_bb.get(SystemBBAttributes.Test_key) == (True, "key")
 
 ####################### TEST SUITE DEFINITION ################################
 
@@ -212,54 +246,37 @@ def define_config_test_suite() -> TestSuite:
     config_test_suite = TestSuite()
     # Blackboard tests 
     config_test_suite.add_test(
-        "blackboard_set", (),True,True,blackboard_set)
+        "blackboard_is_configured_true", (),True,True,blackboard_is_configured_true)
+    config_test_suite.add_test(
+        "blackboard_is_configured_false", (),True,True,blackboard_is_configured_false)
+    config_test_suite.add_test(
+        "blackboard_no_configure_with_bad_data", (),True,True,blackboard_no_configure_with_bad_data)
     config_test_suite.add_test(
         "blackboard_set_invalid_key", (),True,True,blackboard_set_invalid_key)
     config_test_suite.add_test(
         "blackboard_set_valid_key", (),True,True,blackboard_set_valid_key)
     config_test_suite.add_test(
-        "blackboard_get", (), True, True, blackboard_get)
+        "blackboard_get_configured_key", (), True, True, 
+         blackboard_get_configured_key)
     config_test_suite.add_test(
-        "blackboard_get_valid_unset_key", (), True, True, 
-         blackboard_get_valid_unset_key)
-    config_test_suite.add_test(
-        "blackboard_get_valid_set_key", (), True, True, 
-         blackboard_get_valid_set_key)
+        "blackboard_get_set_key", (), True, True, 
+         blackboard_get_set_key)
     config_test_suite.add_test(
         "blackboard_get_invalid_key", (), True, True, 
          blackboard_get_invalid_key)
     # Config tests 
     config_test_suite.add_test(
-        "config_load_from_file",(), True, True, config_load_from_file)
+        "config_load_into_nonexistent_board", (),True,True,config_load_into_nonexistent_board)
     config_test_suite.add_test(
-        "config_load_from_valid_nonexisting_file",(), True, True, 
-         config_load_from_valid_nonexisting_file)
+        "config_load_into_existent_board", (),True,True,config_load_into_existent_board)
     config_test_suite.add_test(
-        "config_load_from_invalid_nonexisting_file",(), True, True, 
-         config_load_from_invalid_nonexisting_file)
+        "config_load_with_bad_data", (),True,True,config_load_with_bad_data)
     config_test_suite.add_test(
-        "config_load_from_valid_existing_file",(), True, True, 
-         config_load_from_valid_existing_file)
+        "config_load_with_bad_key", (),True,True,config_load_with_bad_key)
     config_test_suite.add_test(
-        "config_load_from_invalid_existing_file",(), True, True, 
-         config_load_from_invalid_existing_file)
+        "config_get_nonexistent_board", (),True,True,config_get_nonexistent_board)
     config_test_suite.add_test(
-        "config_load_from_invalid_key_file",(), True, True, 
-         config_load_from_invalid_key_file)
+        "config_get_existent_unloaded_board", (),True,True,config_get_existent_unloaded_board)
     config_test_suite.add_test(
-        "config_load_from_invalid_JSON",(), True, True, 
-         config_load_from_invalid_JSON)
-    config_test_suite.add_test(
-        "config_load_from_incorrect_format_file",(), True, True, 
-         config_load_from_incorrect_format_file)
-    config_test_suite.add_test(
-        "config_load_from_JSON_with_extra_keys",(), True, True, 
-         config_load_from_JSON_with_extra_keys)
-    config_test_suite.add_test(
-        "config_load_from_JSON_with_duplicates",(), True, True, 
-         config_load_from_JSON_with_duplicates)
-    config_test_suite.add_test(
-        "config_get_blackboard",(), True, True, config_get_blackboard)
-    config_test_suite.add_test(
-        "config_blackboard_deepcopy",(), True, True, config_blackboard_deepcopy)
+        "config_get_loaded_board", (),True,True,config_get_loaded_board)
     return config_test_suite
