527a9ddf9bc66157b13fe0f81dc850bbf6422e10
Rosa Vitiello <rosanna.m.vitiello@gmail.com>
2021-02-16 19:47:25 -0500

    Network unit tests

--- Tests/scripts/test_network.py
+++ Tests/scripts/test_network.py
@@ -3,6 +3,7 @@ testing script for the network component
 """
 # Standard library imports 
 from queue import Queue
+
 # Local imports 
 from Src.Components.network import Request, WSInterface, WSProtocolAttributes, \
                                 WebsocketProtocolModel,Network
@@ -63,6 +64,136 @@ def request_send_request() -> bool:
         not request.send_request("GET",invalid_url, empty_data_dict)[0] and \
         not request.send_request("GET", get_url,{"invalid key" : None })[0] and \
         not request.send_request("GET",invalid_url,{"invalid key" : None })[0]
+
+def request_send_request_get() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+
+    get_url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
+    success, req_dict = request.send_request("GET",get_url,empty_data_dict)
+    return success and req_dict["status_code"] == 200
+
+def request_send_request_post() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+    
+    post_url = "https://postman-echo.com/post"
+    success, req_dict = request.send_request("POST",post_url,empty_data_dict)
+    return success and req_dict["status_code"] == 200
+
+def request_send_request_options() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+    
+    options_url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
+    success, req_dict = request.send_request("OPTIONS",options_url,empty_data_dict)
+    return success and req_dict["status_code"] == 200
+
+def request_send_request_put() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+    
+    put_url = "https://postman-echo.com/put"
+    success, req_dict = request.send_request("PUT",put_url,empty_data_dict)  
+    return success and req_dict["status_code"] == 200
+
+def request_send_request_patch() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+    
+    patch_url = "https://postman-echo.com/patch"
+    success, req_dict = request.send_request("PATCH",patch_url,empty_data_dict)
+    return success and req_dict["status_code"] == 200
+
+def request_send_request_delete() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+    
+    delete_url = "https://postman-echo.com/delete"
+    success, req_dict = request.send_request("DELETE",delete_url,empty_data_dict)
+    return success and req_dict["status_code"] == 200
+
+def request_send_bad_request_invalid_url() -> bool:
+    request = Request()
+    empty_data_dict = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+
+    invalid_url = "https://invalid.com"
+    success, _ = request.send_request("GET",invalid_url, empty_data_dict)
+    return not success
+
+def request_send_bad_request_invalid_keys() -> bool:
+    request = Request()
+    invalid = {
+        "bad" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+
+    valid_url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
+    success, _ = request.send_request("GET",valid_url, invalid)
+    return not success
+
+def request_send_bad_request_invalid_url_keys() -> bool:
+    request = Request()
+    invalid = {
+        "bad" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+
+    invalid_url = "https://invalid.com"
+    success, _ = request.send_request("GET",invalid_url, invalid)
+    return not success
+
+def request_send_bad_request_invalid_request_type() -> bool:
+    request = Request()
+    invalid = {
+        "params" : {},
+        "data" : {},
+        "json" : {},
+        "auth" : {},
+        "headers" : {}}
+
+    valid_url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
+    success, req_dict = request.send_request("BAD_GET",valid_url, invalid)
+    return not success and all([v == None for v in req_dict.values()])
 
 ####################### WebsocketProtocolModel test
 def websocket_protocol_model_set() -> bool:
@@ -194,6 +325,19 @@ def WSInterface_set() -> bool:
             ws_interface.set_data_queue(data_queue) and \
             not ws_interface.set_num_threads(2000) and \
             not ws_interface.set_callbacks({})
+    
+def WSInterface_set_bad_callbacks() -> bool:
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "bad" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    
+    ws_interface = WSInterface(ws_test_url, ws_test_headers)
+    return not ws_interface.set_callbacks(callbacks)
 
 def WSInterface_get() -> bool:
     """
@@ -217,7 +361,7 @@ def WSInterface_get() -> bool:
     return type(configs) == dict and \
         len(configs.keys()) == len(expected_keys) and \
         all([ k for k in configs.keys() if k in expected_keys ])
-    
+
 def WSInterface_open_connection() -> bool:
     """
     Tests the open_connection_until_complete WSInterface class that uses 
@@ -261,6 +405,59 @@ def WSInterface_open_connection() -> bool:
     success_2 = ws_interface.open_connection_until_complete(True)
     return success_1 and success_2
 
+def WSInterface_open_connection_valid_no_daemon() -> bool:
+    num_queue_items = 50
+    thread_count = 50
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "on_connect" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    daemon_test_queue = Queue()
+    test_queue = Queue()
+    for i in range(num_queue_items):
+        daemon_test_queue.put(("Test string {}".format(i)))
+        test_queue.put(("Test string {}".format(i)))
+    
+    ws_interface = WSInterface(ws_test_url, ws_test_headers)
+    ws_interface.set_num_threads(thread_count)
+    ws_interface.set_data_queue(test_queue)
+    ws_interface.set_callbacks(callbacks)
+
+    success = ws_interface.open_connection_until_complete(False)
+
+    return success
+
+def WSInterface_open_connection_valid_daemon() -> bool:
+    num_queue_items = 50
+    thread_count = 50
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "on_connect" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    daemon_test_queue = Queue()
+    test_queue = Queue()
+    for i in range(num_queue_items):
+        daemon_test_queue.put(("Test string {}".format(i)))
+        test_queue.put(("Test string {}".format(i)))
+    
+    ws_interface = WSInterface(ws_test_url, ws_test_headers)
+    ws_interface.set_num_threads(thread_count)
+    ws_interface.set_data_queue(test_queue)
+    ws_interface.set_callbacks(callbacks)
+
+    ws_interface.set_data_queue(daemon_test_queue)
+    success = ws_interface.open_connection_until_complete(True)
+    
+    return success
+
 ########################### Network tests
 
 def network_websocket_connection() -> bool:
@@ -290,6 +487,82 @@ def network_websocket_connection() -> bool:
         url=ws_test_url, headers=ws_test_headers, num_threads= thread_count,
         tasks_data=tasks,is_daemon=True,callbacks=callbacks)
 
+def network_websocket_connection_bad_threads() -> bool:
+    network = Network()
+    num_tasks = 50
+    thread_count = 0
+    tasks = list()
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "on_connect" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    for i in range(num_tasks):
+        tasks.append("Task {}".format(i))
+    return not network.websocket_connect(
+        url=ws_test_url, headers=ws_test_headers, num_threads= thread_count,
+        tasks_data=tasks,is_daemon=True,callbacks=callbacks)
+
+def network_websocket_connection_bad_callback() -> bool:
+    network = Network()
+    num_tasks = 50
+    thread_count = 50
+    tasks = list()
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "on_connect" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    for i in range(num_tasks):
+        tasks.append("Task {}".format(i))
+    return not network.websocket_connect(
+        url=ws_test_url, headers=ws_test_headers, num_threads= thread_count,
+        tasks_data=tasks,is_daemon=True,callbacks=callbacks)
+
+def network_websocket_connection_bad_callback() -> bool:
+    network = Network()
+    num_tasks = 50
+    thread_count = 50
+    tasks = list()
+    ws_test_url =  "wss://echo.websocket.org"
+    ws_test_headers = {}
+    callbacks = {
+        "bad" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    for i in range(num_tasks):
+        tasks.append("Task {}".format(i))
+    return not network.websocket_connect(
+        url=ws_test_url, headers=ws_test_headers, num_threads= thread_count,
+        tasks_data=tasks,is_daemon=True,callbacks=callbacks)
+
+def network_websocket_connection_bad_url() -> bool:
+    network = Network()
+    num_tasks = 50
+    thread_count = 50
+    tasks = list()
+    ws_test_url =  "wss://foobar.org"
+    ws_test_headers = {}
+    callbacks = {
+        "on_connect" : on_connect_callback,
+        "on_connecting" : on_connecting_callback,
+        "on_message" : on_message_callback,
+        "on_open" : on_open_callback,
+        "on_close" : on_close_callback}
+    for i in range(num_tasks):
+        tasks.append("Task {}".format(i))
+    return not network.websocket_connect(
+        url=ws_test_url, headers=ws_test_headers, num_threads= thread_count,
+        tasks_data=tasks,is_daemon=True,callbacks=callbacks)
+
 ####################### TEST SUITE DEFINITION ################################
 
 def define_network_test_suite() -> TestSuite:
@@ -302,19 +575,41 @@ def define_network_test_suite() -> TestSuite:
     suite = TestSuite()
     # Request tests 
     suite.add_test("request_send_request", (), True, True, request_send_request)
+    suite.add_test("request_send_request_get", (), True, True, request_send_request_get)
+    suite.add_test("request_send_request_options", (), True, True, request_send_request_options)
+    suite.add_test("request_send_request_post", (), True, True, request_send_request_post)
+    suite.add_test("request_send_request_put", (), True, True, request_send_request_put)
+    suite.add_test("request_send_request_patch", (), True, True, request_send_request_patch)
+    suite.add_test("request_send_request_delete", (), True, True, request_send_request_delete)
+    suite.add_test("request_send_bad_request_invalid_url", (), True, True, request_send_bad_request_invalid_url)
+    suite.add_test("request_send_bad_request_invalid_keys", (), True, True, request_send_bad_request_invalid_keys)
+    suite.add_test("request_send_bad_request_invalid_url_keys", (), True, True, request_send_bad_request_invalid_url_keys)
+    suite.add_test("request_send_bad_request_invalid_request_type", (), True, True, request_send_bad_request_invalid_request_type)
+
     # WebsocketProtocolModel tests 
     suite.add_test("websocket_protocol_model_set", (), True, True, 
-        websocket_protocol_model_set)
+       websocket_protocol_model_set)
     suite.add_test("websocket_protocol_model_get", (), True, True, 
         websocket_protocol_model_get)
     # WSInterface tests
     suite.add_test("WSInterface_set",(),True,True,WSInterface_set)
+    suite.add_test("WSInterface_set_bad_callbacks",(),True,True,WSInterface_set_bad_callbacks)
     suite.add_test("WSInterface_get", (), True, True, WSInterface_get)
-    suite.add_test("WSInterface_open_connection",
-                        (), True, True, WSInterface_open_connection)
+    # suite.add_test("WSInterface_open_connection",
+    #                   (), True, True, WSInterface_open_connection)
+    suite.add_test("WSInterface_open_connection_valid_no_daemon",
+                       (), True, True, WSInterface_open_connection_valid_no_daemon)
+    suite.add_test("WSInterface_open_connection_valid_daemon",
+                        (), True, True, WSInterface_open_connection_valid_daemon)
     # Network tests
     suite.add_test("network_websocket_connection", (), True, True,
         network_websocket_connection)
+    suite.add_test("network_websocket_connection_bad_threads", (), True, True,
+         network_websocket_connection_bad_threads)
+    suite.add_test("network_websocket_connection_bad_callback", (), True, True,
+         network_websocket_connection_bad_callback)
+    # suite.add_test("network_websocket_connection_bad_url", (), True, True,
+    #     network_websocket_connection_bad_url)
     return suite
 
 
