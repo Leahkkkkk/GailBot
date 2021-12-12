52066990eb4e199cd3681d3d962a495136d3dcad
Umair <muhammad.umair@tufts.edu>
2021-12-12 15:22:32 -0500

    Added basic interface

--- Src/interface.py
+++ Src/interface.py
@@ -2,7 +2,7 @@
 # @Author: Muhammad Umair
 # @Date:   2021-12-09 18:01:00
 # @Last Modified by:   Muhammad Umair
-# @Last Modified time: 2021-12-09 18:18:19
+# @Last Modified time: 2021-12-12 15:05:34
 
 from .interfaces import CLIController
 
@@ -18,4 +18,4 @@ class Interface:
         try:
             self.interfaces[interface]().run()
         except Exception as e:
-            print("INTERFACE ERROR")
+            print("INTERFACE ERROR: {}".format(e))
---
+++ Src/interfaces/cli_basic/vars.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2021-12-12 14:42:51
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2021-12-12 14:42:55
