2017556b12530c32b45644bcfee170bde524e212
Muhammad Umair <umair.aarij@gmail.com>
2022-08-23 10:04:37 -0400

    Initial restructuring into plugins and core

---
+++ GailBot/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2021-12-31 13:52:10
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2022-08-23 09:45:49
+
+
+# Package version
+__version__ = '0.1.a6dev'
+
+# pypi info
+__latest_version__ = None
+__current_version_is_outdated__ = False
+__latest_version_upload_time__ = None
+__latest_version_check_time__ = None
---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Muhammad Umair
+# @Date:   2022-08-23 09:38:56
+# @Last Modified by:   Muhammad Umair
+# @Last Modified time: 2022-08-23 09:39:03
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
