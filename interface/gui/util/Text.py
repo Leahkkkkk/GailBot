4b138f5f9eb6fcf7f38863c4d6deeedf0032da96
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2022-12-13 16:52:23 -0500

    change cancel button to red

--- interface/gui/util/Text.py
+++ interface/gui/util/Text.py
@@ -88,7 +88,7 @@ LogDeleteTimeDict = forms["log deletion"]
 @dataclass
 class Links: 
     link = text["links"]["HILAB"]
-    _linkTemplate = "<a style='color:#7A999E; font-weight: 400;' href={0}>{1}</a>"
+    _linkTemplate = "<a style='color:#3B5D78; font-weight: 500;' href={0}>{1}</a>"
     tutorialLink = _linkTemplate.format(
         link, WelcomePageText.tutorialText)
     guideLink = _linkTemplate.format(
