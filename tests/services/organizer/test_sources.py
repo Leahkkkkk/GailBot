c88e723c1fb47c36ac789010fc1b5d7e565e7926
siarasmall <41011312+siarasmall@users.noreply.github.com>
2023-02-20 15:05:24 -0500

    update the test for sources

--- tests/services/organizer/test_sources.py
+++ tests/services/organizer/test_sources.py
@@ -1,11 +1,20 @@
 from gailbot.services.organizer.source import SourceObject, SourceManager
-from gailbot.configs.confs.toplevel import 
+# from gailbot.configs.confs.toplevel import 
+from gailbot.core.utils.general import get_name
+
+#TODO
+SOURCE_PATH = None
+OUTPUT_PATH = None
 
 ## test source object
 
 def test_source_details():
-    test_source =
-    pass
+    test_source_manager = SourceManager()
+    test_source_manager.add_source(SOURCE_PATH, OUTPUT_PATH)
+    name = get_name(SOURCE_PATH)
+    test_source = test_source_manager.get_source(name)
+    details = test_source.source_details(test_source)
+    print (f"source_name: {details.source_name}, source_path: {details.source_path}, settings: {details.settings}" )
 
 def test_configured():
     pass
@@ -22,9 +31,17 @@ def test_is_source():
     pass
 
 def test_source_names():
-    pass
+    #TODO make different source paths
+    test_source_manager = SourceManager()
+    test_source_manager.add_source(SOURCE_PATH, OUTPUT_PATH)
+    test_source_manager = SourceManager()
+    test_source_manager.add_source(SOURCE_PATH, OUTPUT_PATH)
+    test_source_manager = SourceManager()
+    test_source_manager.add_source(SOURCE_PATH, OUTPUT_PATH)
+    names = test_source_manager.source_names(test_source_manager)
 
 def test_get_source():
+    # implicitly tested
     pass
 
 def test_apply_setting_profile_to_source():
