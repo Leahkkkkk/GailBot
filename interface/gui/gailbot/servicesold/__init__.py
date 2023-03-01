de4cc55ecb931adfeb17ab46e92713462e0fcffa
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-03-01 12:03:00 -0500

    update

--- interface/gui/gailbot/servicesold/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Muhammad Umair
-# @Date:   2023-01-08 13:47:55
-# @Last Modified by:   Muhammad Umair
-# @Last Modified time: 2023-01-10 14:44:32
-
-from .controller import GailBotController
-from .pipeline import GBPluginMethods
