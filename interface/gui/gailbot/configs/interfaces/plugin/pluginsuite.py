4e5153c19d7c640318276dd7e89a36950fae9926
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-04-11 17:35:24 -0400

    before adding the encrypted key

--- interface/gui/gailbot/configs/interfaces/plugin/pluginsuite.py
+++ interface/gui/gailbot/configs/interfaces/plugin/pluginsuite.py
@@ -3,8 +3,11 @@ from dataclasses import dataclass
 @dataclass
 class PLUGIN_CONFIG:
     HILAB_BUCKET = "gailbot-plugin"
+    ENCRYPTED_API_KEY = b"yrTw50nWlIzTtSLKplGdYTkPjeGHgChIdjDKkj7S9Fs="
+    EN_KEY = b"gAAAAABkNdIg3vorysUl8XC81I3KeIgnvAVI281wPp0KoOfyR1fnbKyu5Vz8ToTd4nvHU2fWmPgHxA3prRZAQp0bzJr93SsmxtprexkMB-vazN2Fuscs4YE="
     THREAD_NUM = 1
     DOCUMENT = "DOCUMENT.md"
     REQUIREMENT = "requirement.txt"
     CONFIG = "config.toml"
     HILAB_SUITE_ZIP = "https://gailbot-plugin.s3.us-east-2.amazonaws.com/gb_hilab_suite.zip" 
+
--- interface/gui/tests/services/test_data/setting_data.py
+++ interface/gui/tests/services/test_data/setting_data.py
@@ -6,7 +6,6 @@ WATSON_LANG_CUSTOM_ID  = "41e54a38-2175-45f4-ac6a-1c11e42a2d54"
 WATSON_REGION          = "dallas"
 WATSON_BASE_LANG_MODEL = "en-US_NarrowbandModel"
 
-
 """ 
 test data 
 setting: 
