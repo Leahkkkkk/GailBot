05427a044236652d57b65cc3f1173e5408feab2f
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-04-30 18:46:27 -0400

    update

--- plugin_suite/tests/analysis/test_syllable_rate.py
+++ plugin_suite/tests/analysis/test_syllable_rate.py
@@ -1,24 +1,5 @@
-import logging 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-
-from gailbot.plugins import GBPluginMethods
-from gb_hilab_suite.src.core.word_tree import WordTreePlugin
-from gb_hilab_suite.src.core.utterance_map import UtteranceMapPlugin
-from gb_hilab_suite.src.core.speaker_map import SpeakerMapPlugin
-from gb_hilab_suite.src.core.conversation_map import ConversationMapPlugin
-from gb_hilab_suite.src.core.nodes import Node
-from gb_hilab_suite.src.core.conversation_model import ConversationModelPlugin, ConversationModel
-
 from gb_hilab_suite.src.analysis.syllable_rate import SyllableRatePlugin
-from gb_hilab_suite.src.format.chat import ChatPlugin
-from gb_hilab_suite.src.format.text import TextPlugin
-from gb_hilab_suite.src.format.csv import CSVPlugin
-from gb_hilab_suite.src.format.xml import XMLPlugin
 from .test_fun import analysis_test
-from typing import Dict, Union, Any, List, TypedDict
-from pydantic import BaseModel 
 SYLLABLE_RATE_OUT_PATH = "/Users/yike/Desktop/plugin_output/syllable_rate"
 
 def test_syllable_rate():
