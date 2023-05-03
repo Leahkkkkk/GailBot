30478193b988d20e1a7bd10b73c043b31d6520f8
Muhammad Umair <umair.aarij@gmail.com>
2023-05-03 09:32:12 -0400

    Minor changes to plugin configs

--- plugin_suite/gb_hilab_suite/src/configs/__init__.py
+++ plugin_suite/gb_hilab_suite/src/configs/__init__.py
@@ -1,2 +1,13 @@
-from .config import OUTPUT_FILE, load_label, load_threshold, PLUGIN_NAME, INTERNAL_MARKER
-from .formatter import MARKER_FORMATTER, CHAT_FORMATTER, CSV_FORMATTER, CON_FORMATTER
+from .config import (
+    OUTPUT_FILE,
+    load_label,
+    load_threshold,
+    PLUGIN_NAME,
+    INTERNAL_MARKER,
+)
+from .formatter import (
+    MARKER_FORMATTER,
+    CHAT_FORMATTER,
+    CSV_FORMATTER,
+    CON_FORMATTER,
+)
