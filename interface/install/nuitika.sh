6b3b765c6f06c0c6e3fbcaaf087041762828d3b2
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-05-08 00:41:02 -0400

    update

--- interface/install/nuitika.sh
+++ interface/install/nuitika.sh
@@ -4,4 +4,5 @@ python -m nuitka --include-data-dir=../gui/asset=asset \
  --include-data-dir=../gui/config_frontend=config_frontend \
  --windows-icon-from-ico=GailBotLogo.icns \
  --plugin-enable=numpy \
+ --noinclude-numba-mode=nofollow \
  --standalone ../gui/app.py
