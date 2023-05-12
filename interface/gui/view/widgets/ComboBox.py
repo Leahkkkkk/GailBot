37a13c843a6439f64e80ed2517a74484989118bf
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-05-12 14:53:42 -0400

    add font change to combobox

--- interface/gui/view/widgets/ComboBox.py
+++ interface/gui/view/widgets/ComboBox.py
@@ -10,6 +10,7 @@ Modified By:  Siara Small  & Vivian Li
 '''
 from ..config.Style import STYLE_DATA
 from PyQt6.QtWidgets import QComboBox 
+from PyQt6.QtGui import QFont
 
 class ComboBox(QComboBox):
     """ a customized QComboBox Widget """
@@ -20,8 +21,14 @@ class ComboBox(QComboBox):
         self.setFixedHeight(STYLE_DATA.Dimension.COMBOBOX_HEIGHT)
         self.setMinimumWidth(STYLE_DATA.Dimension.INPUTWIDTH)
         STYLE_DATA.signal.changeColor.connect(self.colorChange)
+        STYLE_DATA.signal.changeFont.connect(self.fontchange)
+        font = QFont(STYLE_DATA.FontFamily.OTHER, int(STYLE_DATA.FontSize.BODY[0:-2]))
+        self.setFont(font)
         self.setContentsMargins(0,0,0,0)
          
     def colorChange(self):
         self.setStyleSheet(STYLE_DATA.StyleSheet.COMBO_BOX + f"QComboBox QAbstractItemView {{font-size: {STYLE_DATA.FontSize.BODY}}}")
-    
+   
+    def fontchange(self): 
+        font = QFont(STYLE_DATA.FontFamily.OTHER, int(STYLE_DATA.FontSize.BODY[0:-2]))
+        self.setFont(font)
