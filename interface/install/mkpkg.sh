b0f7e39f31b30ecd8291540d8d3859a804b7265f
Yike Li Viv <86906727+YikeLi-Vivi@users.noreply.github.com>
2023-05-13 08:01:46 -0400

    update

--- interface/install/mkpkg.sh
+++ interface/install/mkpkg.sh
@@ -1,22 +1,5 @@
-NAME="GailBot-apple"
-
-IDENTIFIER="com.pkg.GailBot-apple"
-
-VERSION="0.0.1a1"
-
-INSTALL_LOCATION="/Applications"
-ROOT_LOCATION="/Applications/GailBot.app"
-
-
-# put any command for changing the ownership or permissions here
-chmod -R +x "$ROOT_LOCATION"
-
-# Build package.
-/usr/bin/pkgbuild \
-    --root "$ROOT_LOCATION" \
-    --install-location "$INSTALL_LOCATION" \
-    --component-plist component.plist\
-    --identifier "$IDENTIFIER" \
-    --version "$VERSION" \
-    --ownership preserve \
-    "$NAME.pkg"
+#! /bin/bash 
+chmod +x post
+productbuild --component /Applications/GailBot.app /Applications \
+             --scripts /Users/yike/Documents/GitHub/GailBot/interface/install/post \
+             /Users/yike/Desktop/GailBot-apple.pkg 
